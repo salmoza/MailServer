814cf9c65df082536226d54bd148b809cf1317ac
AbdelrahmanIbrahim11 <abdelrhmanibrahim8080@gmail.com>
2025-12-05 03:21:08 +0200

    changes

---
+++ backend/src/main/java/com/example/backend/dtos/UserSigninDTO.java
@@ -0,0 +1,14 @@
+package com.example.backend.dtos;
+
+
+public class UserSigninDTO {
+
+    private String email ;
+    private String password ;
+
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+
+    public String getPassword() { return password; }
+    public void setPassword(String password) { this.password = password; }
+}
---
+++ backend/src/main/java/com/example/backend/dtos/UserSignupDTO.java
@@ -0,0 +1,18 @@
+package com.example.backend.dtos;
+
+public class UserSignupDTO {
+
+
+    private String username ;
+    private String email ;
+    private String password ;
+
+    public String getUsername() { return username; }
+    public void setUsername(String username) { this.username = username; }
+
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+
+    public String getPassword() { return password; }
+    public void setPassword(String password) { this.password = password; }
+}
